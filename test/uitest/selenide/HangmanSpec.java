e55ffdc4bb17a098625e7691c4aa68efa5a64a74
Pavel Liakh <pavel.liakh@network.global>
2024-11-27 11:43:39 +0400

    tmp

--- test/uitest/selenide/HangmanSpec.java
+++ test/uitest/selenide/HangmanSpec.java
@@ -1,10 +1,16 @@
 package uitest.selenide;
 
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import uitest.AbstractHangmanTest;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.Condition.*;
@@ -13,9 +19,47 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class HangmanSpec extends AbstractHangmanTest {
   @Before
-  public void startGame() {
-    open("/");
-    $(byText("ENG")).click();
+  public void startGame() throws InterruptedException {
+    open("https://bolt.eu/en-ge/food/");
+    Thread.sleep(5000);
+    var address = $$(By.tagName("input")).findBy(attribute("placeholder","Enter your address"));
+//address.scrollTo();
+    address.setValue("Otar Oniashvillis 30");
+    Thread.sleep(5000);
+    address.sendKeys(Keys.ARROW_DOWN);
+    address.sendKeys(Keys.ENTER);
+    Thread.sleep(6000);
+
+    result();
+  }
+
+  private String result() {
+    ElementsCollection providerCards = $$("div[data-testid='components.ProviderCard.providerRatingBadge']");
+
+    List<Map<String, String>> providerInfoList = providerCards.stream().map(card -> {
+      String name = card.$("span.provider-name-class").getText();
+      String rating = card.$("span:nth-of-type(2)").getText();
+      String reviews = card.$("span:nth-of-type(3)").getText();
+      String imageUrl = card.parent().$("img").getAttribute("src");
+
+      return Map.of(
+              "Name", name,
+              "Rating", rating,
+              "Reviews", reviews,
+              "Image URL", imageUrl
+      );
+    }).collect(Collectors.toList());
+
+    String formattedText = providerInfoList.stream()
+            .map(info -> String.format("Name: %s, Rating: %s, Reviews: %s, Image URL: %s",
+                    info.get("Name"),
+                    info.get("Rating"),
+                    info.get("Reviews"),
+                    info.get("Image URL")))
+            .collect(Collectors.joining("\n"));
+
+    System.out.println(formattedText);
+    return formattedText;
   }
 
   @Test
