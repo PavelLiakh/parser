a28537e99b09ced2684929a367b1defdbe67e172
Pavel Liakh <pavel.liakh@network.global>
2025-01-29 19:42:08 +0400

    select all shops

--- test/uitest/DownloadFood.java
+++ test/uitest/DownloadFood.java
@@ -1,79 +1,29 @@
 package uitest;
 
-import com.codeborne.selenide.ElementsCollection;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 import static com.codeborne.selenide.Condition.attribute;
-import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.open;
 import static uitest.Params.longDelay;
 import static uitest.Params.shortDelay;
 
 public class DownloadFood extends AbstractHangmanTest {
+    List<FoodDataWriter.FoodRecord> foodRecords = new ArrayList<>();
+
     @Before
     public void prepare_food_download() {
     }
 
     @Test
     public void download_food_bolt() throws InterruptedException {
-        open("https://bolt.eu/en-ge/food/");
-        Thread.sleep(5000);
         setBoltAddress();
-
-        ElementsCollection providerCards = $$("div[data-testid='components.ProviderCard.view']");
-
-        List<Map<String, String>> providerInfoList = providerCards.stream().map(card -> {
-
-            try {
-                if (card.$("span[data-testid='components.ProviderCard.providerName']").exists()
-                        && card.$("div[data-testid='components.ProviderCard.providerRatingBadge'] span:nth-of-type(2)").exists()
-                        && card.$("div[data-testid='components.ProviderCard.providerRatingBadge'] span:nth-of-type(3)").exists()
-                        && card.parent().$("img").exists()) {
-                } else {
-                    return Map.of("error", "Some elements are missing");
-                }
-
-                String name = card.$("span[data-testid='components.ProviderCard.providerName']").getText();
-
-                String rating = card.$("div[data-testid='components.ProviderCard.providerRatingBadge'] span:nth-of-type(2)").getText();
-                String reviews = card.$("div[data-testid='components.ProviderCard.providerRatingBadge'] span:nth-of-type(3)").getText();
-                String imageUrl = card.parent().$("img").getAttribute("src");
-
-                var info = Map.of(
-                        "Name", name,
-                        "Rating", rating,
-                        "Reviews", reviews,
-                        "Image URL", imageUrl
-                );
-                var print = String.format("Name: %s, Rating: %s, Reviews: %s, Image URL: %s",
-                        info.get("Name"),
-                        info.get("Rating"),
-                        info.get("Reviews"),
-                        info.get("Image URL"));
-                System.out.println(print);
-                return info;
-            } catch (Exception e) {
-                return Map.of("error", e.getMessage());
-            }
-        }).collect(Collectors.toList());
-
-        String formattedText = providerInfoList.stream()
-                .map(info -> String.format("Name: %s, Rating: %s, Reviews: %s, Image URL: %s",
-                        info.get("Name"),
-                        info.get("Rating"),
-                        info.get("Reviews"),
-                        info.get("Image URL")))
-                .collect(Collectors.joining("\n"));
-
-        System.out.println(formattedText);
     }
 
     @Test
@@ -98,6 +48,16 @@ public class DownloadFood extends AbstractHangmanTest {
 //        $$(By.xpath(String.format("//input[@id=//label[text()='%s']/@for]", "Delivery price"))).get(0).click();
 
         $$(By.xpath("//button[@data-test-id='closeFilterButton']")).get(0).click();
+
+        downloadWoltFood();
+    }
+
+    private void downloadWoltFood() {
+        var allShops = $$(By.xpath("//*[@data-test-id='VenueVerticalListGrid']")).get(0);
+
+
+//        FoodDataWriter.writeToCSV(foodRecords);
+        int a =0;
 
     }
 
